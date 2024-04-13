4f8b36bd8c1c8ee60779c9a9cf49e4aa53462ba7
Vasundhara Boominathan <vasundharaboomi@gmail.com>
2024-04-13 06:27:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,9 @@ translator = Translator()
 def main(): 
     pygame.mixer.quit()  # Initialize the mixer module.
     st.set_page_config(page_title='Multilingual Virtual Classroom Assistant', page_icon='📚')   # Set page title and icon
-    page = st.sidebar.radio('Explore', ['Home', 'Comprehension Check', 'Text Language Translator'])
+    page = st.sidebar.radio('Explore', [ 'Comprehension Check', 'Text Language Translator'])
 
-    if page == 'Home':
-        st.title('Welcome to the Multilingual Virtual Classroom Assistant!')
-        st.write('Our app aims to revolutionize the way students learn by providing real-time translation services, comprehension checks, and accessibility features.')
-        st.image('https://murf.ai/resources/media/posts/111/34370533_2203.q702.012.F.m005.c7.language-course-2.jpg', width=680, )
-        st.write('Developed by Team TNcpl026 | 2024')
-
-    elif page == 'Comprehension Check':
+    if page == 'Comprehension Check':
         comprehension_check.page_1()
 
     elif page == 'Text Language Translator':
